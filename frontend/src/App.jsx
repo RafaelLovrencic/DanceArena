1bdc375055b91fa6c4b508ec56dc2a865e8efdc1
alenjurina <alen.jurina@fer.hr>
2025-10-16 21:21:55 +0200

    Refs #5: Dodan primitivni izgled trake

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Naslovnica from './stranice/naslovnica.jsx'
+import Prijava from './stranice/prijava.jsx'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Naslovnica />} />
+        <Route path="/prijava" element={<Prijava />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/stranice/naslovnica.jsx
+++ frontend/src/stranice/naslovnica.jsx
@@ -1,3 +1,14 @@
+import '../izgled/naslovnica.css'
+import { Link } from 'react-router-dom';
+
 export default function Naslovnica() {
-  return <h1>Naslovnica</h1>;
+  return (
+  <>
+    <nav>
+      <p>DanceArena</p>
+      <button className='Login'><Link to="/prijava" className='loginButton'>Prijava</Link></button>
+    </nav>
+    <h1>Naslovnica</h1>
+  </>
+  )
 }
