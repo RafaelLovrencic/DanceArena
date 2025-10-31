69850e7042e5a75469e9eff4bfba5f78cdba8b21
EmanuelRod1 <emanuel.rod@fer.unizg.hr>
2025-10-31 22:30:46 +0100

    Refs #16: Postavljanje backenda
    Temelj backenda je postavljen, napravljen je
    .json za primitivnu bazu podataka te
    sam definirao routere.

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,17 @@ const express = require('express');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Dobrodošli na našu testnu stranicu!!!');
-});
+const homeRuter = require('./src/routes/home.routes')
+const upisRuter = require('./src/routes/upis.routes')
+
+app.use('/', homeRuter);
+app.use('/upispodataka', upisRuter);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
