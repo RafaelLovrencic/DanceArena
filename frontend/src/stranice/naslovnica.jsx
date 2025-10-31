3d0b664e4d6c2483e9dc28bd3e558a1a7f4d29a3
alenjurina <alen.jurina@fer.hr>
2025-10-31 21:58:31 +0100

    Refs #11: Popravak dizajna

--- frontend/src/stranice/naslovnica.jsx
+++ frontend/src/stranice/naslovnica.jsx
@@ -9,6 +9,10 @@ export default function Naslovnica() {
     </nav>
     <section className='brziStart'>
       <h1 className='dobrodoslica'>Dobrodošli u DanceArenu!</h1>
+      <div className='precaci'>
+        <button className='svaNatjecanja'>Sva natjecanja</button>
+        <button className='noviDogadaj'>Stvori novi događaj</button>
+      </div>
     </section>
     <div className='obavijesti'>
       <h1 className='obavijestiTekst'>Obavijesti i nova događanja</h1>
