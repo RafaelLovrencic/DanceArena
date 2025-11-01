f077bb652c76d5581b0d4715baf93e89f03255c2
EmanuelRod1 <emanuel.rod@fer.unizg.hr>
2025-11-01 10:28:50 +0100

    Refs #16
    Stavio da server sluša na portu 5001.

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use('/', homeRuter);
 app.use('/upispodataka', upisRuter);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
