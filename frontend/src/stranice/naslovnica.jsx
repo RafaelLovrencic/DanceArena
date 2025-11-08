cf09d90f7eb757ca2aa67deb8e4dbe89c1b2af36
alenjurina <alen.jurina@fer.hr>
2025-11-08 19:03:05 +0100

    Refs #17: maknuta tipka na naslovnici

--- frontend/src/stranice/naslovnica.jsx
+++ frontend/src/stranice/naslovnica.jsx
@@ -12,7 +12,7 @@ export default function Naslovnica() {
       <h1 className='dobrodoslica'>Dobrodošli u DanceArenu!</h1>
       <div className='precaci'>
         <Link to="/natjecanja" className="svaNatjecanja">Sva natjecanja</Link>
-        <button className='noviDogadaj'>Stvori novi događaj</button>
+        {/* <button className='noviDogadaj'>Stvori novi događaj</button> */}
       </div>
     </section>
     <div className='obavijesti'>
