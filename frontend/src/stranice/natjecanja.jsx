521033b64535e41ba46edd5f3d000113b3546b64
KarloKrznar <karlo.krznar@fer.hr>
2025-11-08 12:36:11 +0100

    Refs #17: Spajanje tablice s bazom, brisanje nat.

--- frontend/src/stranice/natjecanja.jsx
+++ frontend/src/stranice/natjecanja.jsx
@@ -1,14 +1,14 @@
 import '../izgled/natjecanja.css'
 import NavigacijskaTraka from './navigacijskatraka.jsx'
-import natjecanjaData from '../data/natjecanjaData.json'
 import DodajNatjecanje from "./suceljeDodajNatjecanje.jsx";
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 export default function Natjecanja() {
-    const [competitions, setCompetitions] = useState(natjecanjaData);
+    const [loading, setLoading] = useState(true);
+    const [competitions, setCompetitions] = useState([]);
     const [pokaziSucelje, setPokaziSucelje] = useState(false);
     const [odabranoNatjecanje, setOdabranoNatjecanje] = useState(null);
-    const [podaciZaUredi, setPodaciZaUredi] = useState(null)
+    const [podaciZaUredi, setPodaciZaUredi] = useState(null);
     const dohvatiPodatkeONatjecanju = async () => {
         if (!odabranoNatjecanje) return;
         const response = await fetch(`http://localhost:5001/natjecanja/${odabranoNatjecanje._id}`);
@@ -16,7 +16,21 @@ export default function Natjecanja() {
         setPodaciZaUredi(data);
         setPokaziSucelje(true);
     };
-    const test = async () => 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:5001/natjecanja');
+                const data = await response.json();
+                setCompetitions(data);
+            } catch (err) {
+                console.error('Greška kod dohvaćanja natjecanja:', err);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+    /*const test = async () => 
     { try 
         { const response = await fetch("http://localhost:5001/natjecanja/690e668b652282d76a7b68fa"); 
             const data = await response.json(); 
@@ -25,7 +39,25 @@ export default function Natjecanja() {
         { 
             console.error("Greška:", err); 
         } 
-    }
+    }*/
+   const obrisiNatjecanje = async () => {
+    try {
+        const response = await fetch (`http://localhost:5001/natjecanja/${odabranoNatjecanje._id}`, {
+            method: "DELETE",
+        });
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.poruka || "Greška pri brisanju natjecanja");
+    };
+    setCompetitions((prev) => 
+        prev.filter((comp) => comp._id !== odabranoNatjecanje._id));
+    setOdabranoNatjecanje(null);
+    alert("Natjecanje uspješno obrisano");
+   } catch (err){
+    console.error("Greška", err);
+    alert("Došlo je do greške pri brisanju natjecanja");
+   };
+};
     return (
     <>
         <nav>
@@ -36,28 +68,30 @@ export default function Natjecanja() {
         </section>
         <section className="sekcija">
             <div className="tablica-container">
-                {competitions.length > 0 ? (
+                {loading ? (
+                    <p className="loader">Učitavanje...</p>
+                ) : competitions.length > 0 ? (
                     <table className="tablica">
                     <thead>
                         <tr>
                             <th>Naziv</th>
                             <th>Datum</th>
                             <th>Mjesto</th>
-                            <th>Kategorija</th>
+                            <th>Stil plesa</th>
                         </tr>
                     </thead>
                     <tbody>
                         {competitions.map((comp) => (
-                            <tr key={comp.id} onClick={() => {
-                            if (odabranoNatjecanje?.id === comp.id) {
+                            <tr key={comp._id} onClick={() => {
+                            if (odabranoNatjecanje?._id === comp._id) {
                             setOdabranoNatjecanje(null); 
                             } else {
                             setOdabranoNatjecanje(comp); 
-                            }}} className={odabranoNatjecanje?.id === comp.id ? 'selected' : ''}>
-                                <td>{comp.naziv}</td>
-                                <td>{comp.datum}</td>
-                                <td>{comp.mjesto}</td>
-                                <td>{comp.kategorija}</td>
+                            }}} className={odabranoNatjecanje?._id === comp._id ? 'selected' : ''}>
+                                <td>{comp.ime}</td>
+                                <td>{new Date(comp.datum).toLocaleDateString('hr-HR')}</td>
+                                <td>{comp.lokacija}</td>
+                                <td>{comp.kategorije?.length || '—'}</td>
                             </tr>
                         ))}
                     </tbody>
@@ -70,7 +104,7 @@ export default function Natjecanja() {
             <div className="gumbovi">
                 <button className="dodaj" onClick={() => setPokaziSucelje(true)}>Dodaj natjecanje</button>
                 <button className="uredi" onClick={dohvatiPodatkeONatjecanju} style={{backgroundColor: odabranoNatjecanje ? '#2CDE32' : 'rgba(23, 101, 25, 1)', cursor: odabranoNatjecanje ? 'pointer' : 'not-allowed'}}>Uredi natjecanje</button>
-                <button className="obrisi" onClick={test}>Obriši natjecanje</button>
+                <button className="obrisi" onClick={obrisiNatjecanje} style={{backgroundColor: odabranoNatjecanje ? '#2CDE32' : 'rgba(23, 101, 25, 1)', cursor: odabranoNatjecanje ? 'pointer' : 'not-allowed'}}>Obriši natjecanje</button>
             </div>
         </section>
         {pokaziSucelje && (
