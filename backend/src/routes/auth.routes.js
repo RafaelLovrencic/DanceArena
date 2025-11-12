cb5810f79ad9e80a7957ade48bbadc23c4757582
RafaelLovrencic <rafael.lovrencic@fer.unizg.hr>
2025-11-12 22:04:48 +0100

    Reroll promjena kod auth cookieja

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -24,7 +24,7 @@ router.get("/google/callback",
       res.cookie("token", token, {
         httpOnly: true,
         maxAge: 7 * 24 * 60 * 60 * 1000,
-        sameSite: "None",
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
         secure: process.env.NODE_ENV === "production", // HTTPS u produkciji
       });
 
