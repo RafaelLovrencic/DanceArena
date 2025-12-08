bc96730914f0f3d10582bdfa808206f35b759fc1
alenjurina <alen.jurina@fer.hr>
2025-12-08 17:14:48 +0100

    Pokusaj popravka problema s cookiejima pomocu micanja competitions u useeffectu

--- frontend/src/stranice/natjecanja.jsx
+++ frontend/src/stranice/natjecanja.jsx
@@ -39,7 +39,7 @@ export default function Natjecanja() {
             }
         };
         fetchData();
-    }, [competitions]);
+    }, []);
 
    const obrisiNatjecanje = async () => {
         if (!odabranoNatjecanje) return;
